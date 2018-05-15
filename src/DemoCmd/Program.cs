931d8797f0f9df431d3ff9a916bb5028bbef2604
Eric Johnson <johnson.r.eric21@gmail.com>
2018-05-15 10:46:08 -0500

    Added getHeightFromCsv method to Program.cs; Updated gitignore

--- src/DemoCmd/Program.cs
+++ src/DemoCmd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using VDatumParser;
+using System;
 
 namespace DemoCmd
 {
@@ -7,11 +8,12 @@ namespace DemoCmd
     {
         static void Main(string[] args)
         {
-            string gtxFilePath = @"C:\VDatum\ALFL_gom02_8301\mllw.gtx";
+            string gtxFilePath = @"C:\VDatum\ALFLgom02_8301\mllw.gtx";
             GtxFile gtxFile = new GtxFileParser().Parse(gtxFilePath);
             gtxFile.ToCsv("mllw.csv");
         }
     }
+    
 
     public static class GtxFileExtensions
     {
@@ -50,6 +52,54 @@ namespace DemoCmd
                     streamWriter.WriteLine();
                 }
             }
+        }
+
+        public static double getHeightFromCsv(this GtxFile gtxFile, string filePath, double latitude, double longitude)
+        {
+            StringReader stringReader = new StringReader(File.Open(filePath,FileMode.Open,FileAccess.Read,FileShare.ReadWrite).ToString());
+            string[] separators = { "," };
+            double nullReturnValue = -88.8888;
+
+            //Read first line (column headers/latitude)
+            string columnHeader = stringReader.ReadLine();
+
+            //Split column header with "," delimiter; convert to double[]
+            double[] columnHeaderValues = Array.ConvertAll(columnHeader.Split(separators,StringSplitOptions.RemoveEmptyEntries),Double.Parse);
+
+            //Find passed latitude in column header values
+            bool inRange = (latitude >= columnHeaderValues[0] && latitude <= columnHeaderValues[columnHeaderValues.Length]);
+            if (!inRange)
+                return nullReturnValue;
+            
+            int valueColumnIndex = (int)((latitude - columnHeaderValues[0]) / gtxFile.DeltaLatitudeDecimalDegrees);
+            double nearestColumnValue = columnHeaderValues[0] + (valueColumnIndex * gtxFile.DeltaLatitudeDecimalDegrees);
+
+            //Get second row
+            string rowString = stringReader.ReadLine();
+            //Split row with "," delimiter, convert to double[]
+            double[] rowValues = Array.ConvertAll(rowString.Split(separators, StringSplitOptions.None), Double.Parse);
+            //Get header value (longitude)
+            double rowHeaderValue = rowValues[0];
+
+            //Check if longitude value is potentially in the range
+            inRange = (longitude >= rowHeaderValue);
+            if (!inRange)
+                return nullReturnValue;
+
+            //calculate index value for matching row header value
+            int valueRowIndex = (int)((longitude - rowValues[0]) / gtxFile.DeltaLongitudeDecimalDegrees);
+            double nearestRowValue = rowValues[0] + (valueRowIndex * gtxFile.DeltaLongitudeDecimalDegrees);
+
+            //get to row with value
+            for (int i = 0; i < valueRowIndex; i++)
+                rowString = stringReader.ReadLine();
+
+            //Split row with "," delimiter, convert to double[]
+            rowValues = Array.ConvertAll(rowString.Split(separators, StringSplitOptions.None), Double.Parse);
+            //Get height value
+            double heightValue = rowValues[valueColumnIndex + 1];
+
+            return heightValue;
         }
     }
 }
